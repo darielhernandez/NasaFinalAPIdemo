316105d25b91373b98c4c8c144566a5b2384f67b
darielhernandez <13dariel@gmail.com>
2022-03-15 18:58:03 -0400

    404 error when trying w/ Fast Request. 75ish minute mark on the March 10th video.

--- src/main/java/com/careerdevs/NasaFinalAPIdemo/Controllers/APODcontroller.java
+++ src/main/java/com/careerdevs/NasaFinalAPIdemo/Controllers/APODcontroller.java
@@ -29,6 +29,7 @@ public class APODcontroller {
         String apodKey= env.getProperty("APOD_KEY", "IfwytowWwR35YpEAuZciPweFCX16uWvUEqPVJCyR");
         String URL= "https://api.nasa.gov/planetary/apod?api_key=" +apodKey;
         Object response= restTemplate.getForObject(URL, Object.class);
+
         System.out.println(response);
         return response;
 
